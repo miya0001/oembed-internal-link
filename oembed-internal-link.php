75c0182aeac8c581a437cf70ec880be0b7077cbf
Takayuki Miyauchi <miya@theta.ne.jp>
2011-10-22 00:44:16 +0900

    0.1.0

---
+++ oembed-internal-link.php
@@ -0,0 +1,83 @@
+<?php
+/*
+Plugin Name: oEmbed Intarnal Link
+Author: Takayuki Miyauchi
+Plugin URI: http://firegoby.theta.ne.jp/wp/oembed-internal-link
+Description: Display thumbnail link from URL.
+Version: 0.1.0
+Author URI: http://firegoby.theta.ne.jp/
+Domain Path: /languages
+Text Domain: oembed-internal-link
+*/
+
+new childPagesShortcode();
+
+new internalLinks();
+
+class internalLinks {
+
+function __construct()
+{
+    wp_embed_register_handler(
+        'internal_link',
+        '#^('.home_url().'/.+)$#i',
+        array(&$this, 'handler')
+    );
+    add_action('wp_head', array(&$this, 'wp_head'));
+    add_shortcode('internal_link', array(&$this, 'display'));
+}
+
+public function wp_head()
+{
+    $url = WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)).'/style.css';
+    printf(
+        '<link rel="stylesheet" type="text/css" media="all" href="%s" />'."\n",
+        apply_filters("oembed-internal-link-stylesheet", $url)
+    );
+}
+
+private function template()
+{
+    $w = get_option("thumbnail_size_w");
+    $h = get_option("thumbnail_size_h");
+
+    $html = '<div class="internal-link internal-link-%post_id%">';
+    $html .= '<div class="post-thumb" style="width:'.intval($w).'px;height:'.intval($h).'px;">';
+    $html .= '<a href="%post_url%">%post_thumb%</a>';
+    $html .= '</div>';
+    $html .= '<div class="post-content">';
+    $html .= '<h4>%post_title%</h4>';
+    $html .= '<div class="post-excerpt">%post_excerpt%</div>';
+    $html .= '<div class="more"><a href="%post_url%">Read More</a></div>';
+    $html .= '</div>';
+    $html .= '</div>';
+    return apply_filters("oembed-internal-link-template", $html);
+}
+
+public function handler($matches, $attr, $url, $rawattr)
+{
+    $args = preg_split("/#/", $matches[1]);
+    return "[internal_link url=\"{$matches[1]}\"]";
+}
+
+public function display($p){
+    if (!isset($p['url']) || !$p['url']) {
+        return ;
+    }
+    $id = url_to_postid($p['url']);
+    $img = get_the_post_thumbnail($id, "thumbnail");
+    $post = get_post($id);
+
+    $tpl = $this->template();
+    $tpl = str_replace('%post_id%', esc_html($id), $tpl);
+    $tpl = str_replace('%post_title%', esc_html($post->post_title), $tpl);
+    $tpl = str_replace('%post_excerpt%', esc_html($post->post_excerpt), $tpl);
+    $tpl = str_replace('%post_thumb%', $img, $tpl);
+    $tpl = str_replace('%post_url%', esc_url($p['url']), $tpl);
+
+    return $tpl;
+}
+
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,68 @@
+=== oEmbed Internal Link ===
+Contributors: miyauchi
+Donate link: http://firegoby.theta.ne.jp/
+Tags: widget 
+Requires at least: 3.2
+Tested up to: 3.2
+Stable tag: 0.1.0
+
+Easy internal link by oEmbed.
+
+== Description ==
+
+Easy internal link by oEmbed.
+
+[This plugin maintained on GitHub.](https://github.com/miya0001/)
+
+= filter hooks example =
+
+Filter for default template.
+
+`<?php
+    add_filter("oembed-internal-link-template", "my_template");
+    function my_template($template) {
+        return '<div class="%class%"><a href="%post_url%">%post_thumb%</a></div>';
+    }
+?>`
+
+You can use tags in the template as below.
+
+* %post_id%
+* %post_url%
+* %post_thumb%
+* %post_excerpt%
+
+
+Filter for stylesheet URI.
+
+`<?php
+    add_filter("oembed-internal-link-stylesheet", "my_style");
+    function my_style($url) {
+        return 'http://example.com/path/to/style.css';
+    }
+?>`
+
+
+= Contributors =
+
+* [Takayuki Miyauchi](http://firegoby.theta.ne.jp/)
+
+== Installation ==
+
+* A plug-in installation screen is displayed on the WordPress admin panel.
+* It installs it in `wp-content/plugins`.
+* The plug-in is made effective.
+
+== Changelog ==
+
+= 0.1.0 =
+* The first release.
+
+== Credits ==
+
+This plug-in is not guaranteed though the user of WordPress can freely use this plug-in free of charge regardless of the purpose.
+The author must acknowledge the thing that the operation guarantee and the support in this plug-in use are not done at all beforehand.
+
+== Contact ==
+
+twitter @miya0001
